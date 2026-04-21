a64393e1fa2067b16f7649e5931754c0eeafd255
Edouard Gardy <edouard.gardy@gmail.com>
2026-04-21 13:51:53 +0200

    correction des erreurs sur get film

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, HTTPException
 from pydantic import BaseModel
 from db import get_connection
 import jwt
@@ -72,20 +72,25 @@ async def register(user : RegisterRequest):
         return res
     
 @app.post("/auth/login")
-async def login(user : LoginRequest):
+async def login(user: LoginRequest):
     with get_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute(f"""
-            SELECT * FROM Utilisateur WHERE Pseudo='{user.pseudo}' AND MotDePasse='{user.password}'
-            """)
+        
+        cursor.execute("""
+            SELECT * FROM "Utilisateur" WHERE AdresseMail=? AND MotDePasse=?
+            """, (user.email, user.password))
         res = cursor.fetchone()
-        jwt_token = create_access_token({"sub": user.pseudo})
-        print(res)
-        return {"access_token": jwt_token, "token_type": "bearer"} 
+        if res is None:
+            raise HTTPException(status_code=401, detail="Email ou mot de passe incorrect")
+
+        pseudo_utilisateur = res[1] 
+        jwt_token = create_access_token({"sub": pseudo_utilisateur})
+        
+        return {"access_token": jwt_token, "token_type": "bearer"}
     
 
 @app.get("/films")
-async def get_film_page(page: int, per_page: int, genre : int | None = None):
+async def get_film_page(page: int = 1, per_page: int = 20, genre: int | None = None):
     with get_connection() as conn:
         cursor = conn.cursor()
         offset = (page - 1) * per_page
@@ -93,7 +98,9 @@ async def get_film_page(page: int, per_page: int, genre : int | None = None):
         if genre is not None:
             cursor.execute(f"SELECT COUNT(*) FROM FILM WHERE Genre_ID = {genre}")
             total = cursor.fetchone()[0]
-            cursor.execute(f"SELECT * FROM FILM WHERE Genre_ID = {genre} LIMIT {per_page} OFFSET {offset}")
+            cursor.execute("""SELECT * FROM Film WHERE Genre_ID = ? ORDER BY DateSortie DESC LIMIT ? OFFSET ?""", (genre_id, per_page, offset))
+
+
         else:
             cursor.execute("SELECT COUNT(*) FROM FILM")
             total = cursor.fetchone()[0]
@@ -114,8 +121,14 @@ def getFilm(film_id: int):
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM Film WHERE Id = ?", (film_id,))
         res = cursor.fetchone()
-        print(res)
+
+        
+        if res is None:
+            # On déclenche l'erreur 404 qui va stopper la fonction
+            raise HTTPException(status_code=404, detail="Film introuvable")
+            
         return res
+        
 
 @app.get("/genres")
 def getGenres():
